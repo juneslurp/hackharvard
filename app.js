24db7c30098e18e263684695f66bcde1172261eb
Janine Ko <jdk2160@columbia.edu>
2015-11-14 12:57:31 -0500

    Quick Start from https://www.npmjs.com/package/twitter. Client.get call for favorites.

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ var express = require('express');
 var app = express();
 var path = require("path");
 
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: 'Jy4IWIkTC3jzyvheRv1fbwIdM',
+  consumer_secret: 'fC7bA2OoyK6Uk8jCUE8BSJluWOGefgpX8lnp33hTeoPJvS2Z25',
+  access_token_key: '1433449044-yGZuglyaUeOkhjj6m15SbQzYbT525uRjDElWGE0',
+  access_token_secret: 'uovmu4FNe01uLl4fhwdzquMFQZQuY6L0BthHr7sWx5W0z'
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+client.get('favorites/list', function(error, tweets, response){
+  if(error) throw error;
+  console.log(tweets);  // The favorites. 
+  console.log(response);  // Raw response object. 
+});
+
+
 app.get('/helloworld', function (req, res) {
   res.send('Hello World!');
 });
