20eb388a018c6eb80c894ad11fb834997b9b1342
James Xue <jamesxue100@gmail.com>
2015-11-15 07:28:13 -0500

    adding express to azure

--- server.js
+++ server.js
@@ -1,15 +1,54 @@
-var http = require('http'),
-    fs = require('fs');
+var express = require('express');
+var app = express();
+var path = require("path");
 
-var port = process.env.PORT || 1337;
+var Twitter = require('twitter');
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(port);
+var client = new Twitter({
+    consumer_key: 'Jy4IWIkTC3jzyvheRv1fbwIdM',
+    consumer_secret: 'fC7bA2OoyK6Uk8jCUE8BSJluWOGefgpX8lnp33hTeoPJvS2Z25',
+    access_token_key: '1433449044-yGZuglyaUeOkhjj6m15SbQzYbT525uRjDElWGE0',
+    access_token_secret: 'uovmu4FNe01uLl4fhwdzquMFQZQuY6L0BthHr7sWx5W0z'
+});
+
+// var MongoClient = require('mongodb').MongoClient;
+// var assert = require('assert');
+
+// var url = 'mongodb://localhost:27017/test';
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server.");
+//   db.close();
+// });
+ 
+// app.get('/',function(req,res){
+//     var ret = "";
+//     var params = {
+//         q: 'flu',
+//         lang:'en',
+//         result_type: 'recent',
+//         geocode:'39.828282,-98.579555,1400mi'
+//     };
+
+//     client.get('search/tweets', params, function(error, tweets, response){
+//         console.log(tweets);
+//         for(i in tweets.statuses) {
+//             ret += tweets.statuses[i].text + "<br><br>";
+//         }
+//         res.send(tweets);
+//     });
+// });
+
+app.get('/',function(req, res){
+  // res.sendFile(path.join(__dirname+'/index.html'));
+  res.send('asdf');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var server = app.listen(process.env.PORT || 3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('HackHarvard app listening at http://%s:%s', host, port);
 });
