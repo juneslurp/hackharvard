66bcc6431ef649f06f7fcfc6bc67d17159327575
James Xue <jamesxue100@gmail.com>
2015-11-15 06:27:57 -0500

    removed mongo to deploy

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ var client = new Twitter({
     access_token_secret: 'uovmu4FNe01uLl4fhwdzquMFQZQuY6L0BthHr7sWx5W0z'
 });
 
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
+// var MongoClient = require('mongodb').MongoClient;
+// var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/test';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
-});
+// var url = 'mongodb://localhost:27017/test';
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server.");
+//   db.close();
+// });
  
 // app.get('/',function(req,res){
 //     var ret = "";
